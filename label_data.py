d6250cc4420bef149d01cf17ce668a0a06dc7be4
Michael Li <li.michael1998@gmail.com>
2020-04-07 21:59:02 -0700

    everything loaded

--- label_data.py
+++ label_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import re
 
 # def filter_dataset(filename):
 #     questions = list(set(pd.read_json(filename, lines=True)["question"]))
@@ -26,7 +27,7 @@ def load_data(state):
     state['DATA'] = pd.read_json(state['DATAFILE'], lines=True).to_dict('records')
     return state
 
-def load_file(state):
+def prompt_and_load_file(state, default='data/questions.json'):
     while state['DATA'] == None:
         path = input('Name of dataset to load?\n')
 
@@ -38,20 +39,56 @@ def load_file(state):
 
     return state
 
+def validate_label(label, existing_labels):
+    regex = re.compile('[@_!#$%^&*()<>?/\|}{~:\s]')
+
+    if label not in existing_labels and regex.search(string) == None:
+        return True
+    else:
+        return False
+
+def load_existing_labels():
+    existing_labels = None
+
+    with open('labels', 'r') as f:
+        existing_labels = f.read().splitlines()
+
+    return set(existing_labels)
+
+def create_new_label(state):
+    label = input('What is the name of the new label?\n')
+
+    while not validate_label(label, existing_labels):
+        label = input("Please enter a valid new label\n")
+
+    with open(labels, 'a') as f:
+        f.write(label+'\n')
+
+    state['LABEL'] = label
+    return state
+
 def load_label(state):
-    print('yes')
-    # while state['LABEL'] == None:
+    existing_labels = load_existing_labels()
 
-        # label = input('type label or n for new label')
+    while state['LABEL'] == None:
+        label = input('type label or n for new label\n')
 
-        
+        if label == 'n':
+            state = create_new_label(state)
+        else:
+            if label in existing_labels:
+                state['LABEL'] = label
+            else:
+                print('Please enter a valid label from the following:')
+                print(existing_labels)
 
+    return state
 
 def main():
     state = {'LABEL': None, 'DATA': None, 'DATAFILE': 'data/questions.json'}
 
     # eventually want to have option to filter data and append to existing dataset
-    state = load_file(state)
+    state = prompt_and_load_file(state)
     state = load_label(state)
 
     while True:
