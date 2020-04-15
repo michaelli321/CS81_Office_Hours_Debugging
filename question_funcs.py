dc61bc019a3dfd326216d65cce77003eaaf96d45
Michael Li <li.michael1998@gmail.com>
2020-04-14 19:59:22 -0700

    actually modularize

--- question_funcs.py
+++ question_funcs.py
@@ -1,7 +1,11 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+
 import pickle
 import utils
 
 def is_actual_question(question, threshold=.4):
-	clf, vectorizer = pickle.load(open('models/actual_question_clf.pkl', 'rb'))
+	clf, vectorizer = pickle.load(open(os.path.join(os.path.dirname(__file__), 'models/actual_question_clf.pkl'), 'rb'))
 	question, _ = utils.preprocess_data([question], vectorizer)
 	return clf.predict_proba(question)[0][1] >= threshold
